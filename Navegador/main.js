f2ff3b29586a5626e174fbc7d9bb1c09389e731e
Yan dos Santos Teixeira <yan.santos12t@gmail.com>
2025-07-31 19:14:16 -0400

    main.js

--- Navegador/main.js
+++ Navegador/main.js
@@ -1,15 +1,87 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, BrowserView, ipcMain, dialog } = require('electron');
+const monitoramento = require('./monitoramento.js'); 
 
-function creatWindow() {
-    const win = new BrowserWindow({
-        wight: 800,
-        height: 600,
+
+
+let mainWindow;
+let tabs = [];
+let currentTab = 0;
+
+
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 700,
         webPreferences: {
-            nodeIntegration: true
+            preload: __dirname + '/preload.js',
+            contextIsolation: true,
+            nodeIntegration: false
         }
-    })
+    });
 
-    win.loadFile('index.html');
+    createNewTab("https://example.com");
+    mainWindow.loadFile('index.html');
+    monitoramento(app, mainWindow, dialog);
+});
+
+function createNewTab(url) {
+    const newView = new BrowserView();
+    newView.webContents.loadURL(url);
+
+    tabs.push(newView);
+    currentTab = tabs.length - 1;
+
+    mainWindow.setBrowserView(newView);
+    setTabBounds();
 }
 
-app.whenReady().then(creatWindow);
+function setTabBounds() {
+    if (tabs[currentTab]) {
+        tabs[currentTab].setBounds({ x: 0, y: 100, width: 1000, height: 600 }); 
+    }
+}
+
+function switchTab(index) {
+    if (tabs[index]) {
+        currentTab = index;
+        mainWindow.setBrowserView(tabs[currentTab]);
+        setTabBounds();
+    }
+}
+
+// === IPC EVENTS ===
+ipcMain.on('navigate', (event, url) => {
+    if (!url.startsWith("http")) {
+        // Se o usuário digitou algo que não é um link, transforma em pesquisa do Google
+        url = "https://www.google.com/search?q=" + encodeURIComponent(url);
+    }
+    if (tabs[currentTab]) {
+        tabs[currentTab].webContents.loadURL(url);
+    }
+});
+
+
+ipcMain.on('back', () => {
+    if (tabs[currentTab]?.webContents.canGoBack()) {
+        tabs[currentTab].webContents.goBack();
+    }
+});
+
+ipcMain.on('forward', () => {
+    if (tabs[currentTab]?.webContents.canGoForward()) {
+        tabs[currentTab].webContents.goForward();
+    }
+});
+
+ipcMain.on('reload', () => {
+    tabs[currentTab]?.webContents.reload();
+});
+
+ipcMain.on('new-tab', (event, url) => {
+    createNewTab(url || "https://example.com");
+});
+
+ipcMain.on('switch-tab', (event, index) => {
+    switchTab(index);
+});
