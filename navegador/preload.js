27c148c6d736a6a1b118dce66c22f692ba66bef2
lucasrocha777 <lucastosin44@gmail.com>
2025-08-05 17:22:01 -0300

    Implementação do sistema de historico, modo escuro adicionado, melhoria de desempenho e interface.

--- navegador/preload.js
+++ navegador/preload.js
@@ -5,6 +5,20 @@ contextBridge.exposeInMainWorld('electronAPI', {
     back: () => ipcRenderer.send('back'),
     forward: () => ipcRenderer.send('forward'),
     reload: () => ipcRenderer.send('reload'),
-    newTab: (url) => ipcRenderer.send('new-tab', url),
-    switchTab: (index) => ipcRenderer.send('switch-tab', index)
+    newTab: () => ipcRenderer.send('new-tab'),
+    switchTab: (index) => ipcRenderer.send('switch-tab', index),
+    closeTab: (index) => ipcRenderer.send('close-tab', index),
+    showHistory: () => ipcRenderer.send('show-history'),
+    getHistory: () => ipcRenderer.invoke('get-history'),
+    clearHistory: () => ipcRenderer.send('clear-history'),
+    setDarkMode: (dark) => ipcRenderer.send('set-dark-mode', dark),
+    setDarkMode: (dark) => ipcRenderer.send('set-dark-mode', dark),
+    getDarkMode: () => ipcRenderer.invoke('get-dark-mode'),
+    onUpdateTheme: (callback) => ipcRenderer.on('update-theme', callback),
+
+    onUpdateTabs: (callback) => ipcRenderer.on('update-tabs', (event, tabList, currentIndex) => {
+        callback(tabList, currentIndex);
+    }),
+
+    onHistoryUpdated: (callback) => ipcRenderer.on('history-updated', callback)
 });
